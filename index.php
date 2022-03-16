06c42ce226ba76a9b8e2eb251bd81aec5080edb0
CamileGhastine <camileghastine@hotmail.com>
2022-03-16 19:35:51 +0100

    display table with hidden cards

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Jeu memory</title>
+</head>
+
+<body>
+
+    <h1>Jeu memory</h1>
+    <div id="table">test</div>
+
+    <SCRIPT language="JavaScript" type="text/JavaScript" src="js/script.js"></SCRIPT>
+</body>
+
+</html>
