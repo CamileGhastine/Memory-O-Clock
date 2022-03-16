d547de367f70607e142ac8127c7fb20613180de6
CamileGhastine <camileghastine@hotmail.com>
2022-03-16 21:45:10 +0100

    show a card

--- js/script.js
+++ js/script.js
@@ -1,24 +1,49 @@
 var divTable = document.querySelector('#table');
 
-arrayTable = [
+var arrayTable = [
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0],
 ]
 
+var arraySolution = [
+    [2, 4, 6, 8, 10, 12, 14],
+    [2, 4, 6, 8, 10, 12, 14],
+    [1, 3, 5, 7, 9, 11, 13],
+    [1, 3, 5, 7, 9, 11, 13],
+]
+
 function displayTable() {
     var table = "<table>"
+    var row = 0
+
     arrayTable.forEach(tr => {
+        var column = 0
         table += "<tr>"
+
         tr.forEach(td => {
-            table += '<td><img src="img/0.png" alt="image de fruit"></td>'
+            table += '<td onClick=show("' + row + column + '")><img src="img/' + td + '.png" alt="image de fruit"></td>'
+
+            column++
         })
+
+        row++
         table += '</tr>'
-    });
+    })
+
     table += '</table>'
-    return table
+
+    divTable.innerHTML = table;
 }
 
-console.log(table);
-divTable.innerHTML = displayTable();
+function show(td) {
+    var rowTd = td.charAt(0)
+    var columnTd = td.charAt(1)
+
+    arrayTable[rowTd][columnTd] = arraySolution[rowTd][columnTd]
+
+    displayTable()
+}
+
+displayTable();
